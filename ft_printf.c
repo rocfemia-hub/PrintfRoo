0a176e94137042e6cba65ca2826b99de4a8b4932
Rocio Femia Ruiz <rocfemia@c2r13s3.42madrid.com>
2024-12-15 18:48:44 +0100

    mejorando el main

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rocfemia <rocfemia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 19:54:56 by roo               #+#    #+#             */
-/*   Updated: 2024/12/13 01:28:25 by rocfemia         ###   ########.fr       */
+/*   Updated: 2024/12/15 18:44:06 by rocfemia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,38 @@ int	ft_printf(char const *str, ...)
 	return (result);
 }
 
-/*int	main(void)
+/*#include <stdio.h>
+int	main(void)
 {
-	int result;
+	char a = 'a';
+	char *str = "Patata";
+	int b = -3;
+	int c = 3;
+	int d = 10;
 
-	result = ft_printf("Patata %s\n", "Frita");
-	ft_printf("%d\n", "El total de letas es: %d\n", result);
+	ft_printf("\n");
+	
+	ft_printf("Roo 1: %c\n", a);
+	ft_printf("Roo 2: %s\n", str);
+	ft_printf("Roo 3: %p\n", str);
+	ft_printf("Roo 4: %i\n", c);
+	ft_printf("Roo 5: %d\n", b);
+	ft_printf("Roo 6: %u\n", b);
+	ft_printf("Roo 7: %x\n", d);
+	ft_printf("Roo 8: %X\n", d);
+	ft_printf("Roo 9: %%\n");
+
+	printf("\n");
+	
+	printf("Original 1: %c\n", a);
+	printf("Original 2: %s\n", str);
+	printf("Original 3: %p\n", str);
+	printf("Original 4: %i\n", c);
+	printf("Original 5: %d\n", b);
+	printf("Original 6: %u\n", b);
+	printf("Original 7: %x\n", d);
+	printf("Original 8: %X\n", d);
+	printf("Original 9: %%\n");
+
 	return (0);
 }*/
